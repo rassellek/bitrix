20729b3523df88ad5561f850a7668c216b6a18e5
= <=>
2023-03-23 13:27:27 +0300

    [ex1 1]

---
+++ s2/partneram/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/s2/partneram/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/s2/partneram/kak-stat-partnerom.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/s2/partneram/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
