057b284fc55fb4677b4ad8e041b911cde6beb301
= <=>
2023-03-24 18:04:16 +0300

    [ex1 15]

---
+++ local/templates/exam1/components/bitrix/news.list/slider/template.php
@@ -0,0 +1,86 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$this->setFrameMode(true);
+?>
+<div class="item-wrap">
+    <div class="rew-footer-carousel">
+
+        <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
+            <?= $arResult["NAV_STRING"] ?><br />
+        <? endif; ?>
+
+        <? foreach ($arResult["ITEMS"] as $arItem): ?>
+            <?
+            $this->AddEditAction(
+                $arItem['ID'], $arItem['EDIT_LINK'],
+                CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT")
+            );
+            $this->AddDeleteAction(
+                $arItem['ID'], $arItem['DELETE_LINK'],
+                CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"),
+                ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]
+            );
+            ?>
+
+            <div class="item">
+                <div class="side-block side-opin" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                    <div class="inner-block">
+                        <div class="title">
+                            <div class="photo-block">
+                                <? if ($arItem["PREVIEW_PICTURE"]): ?>
+                                    <?
+                                    $arImageFile = CFile::ResizeImageGet(
+                                        $arItem["PREVIEW_PICTURE"]["ID"],
+                                        ['width' => 39, 'height' => 39],
+                                        BX_RESIZE_IMAGE_PROPORTIONAL,
+                                        true
+                                    );
+                                    $src = $arImageFile['src'];
+                                    ?>
+                                <? else: ?>
+                                    <?
+                                    $src = SITE_TEMPLATE_PATH . '/img/no_photo_left_block.jpg';
+                                    ?>
+                                <? endif; ?>
+                                <img src="<?= $src ?>" alt="">
+                            </div>
+
+                            <div class="name-block">
+                                <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
+                                    <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
+                                        <?= $arItem["NAME"] ?>
+                                    <? endif; ?>
+                                </a>
+                            </div>
+
+                            <div class="pos-block">
+                                <? //<Должность> ?>
+                                <? if ($arItem["DISPLAY_PROPERTIES"]["POSITION"]["DISPLAY_VALUE"]): ?>
+                                    <?= ucfirst($arItem["DISPLAY_PROPERTIES"]["POSITION"]["DISPLAY_VALUE"]); ?>,
+                                <? endif ?>
+                                <? //<Название компании> ?>
+                                <? if ($arItem["DISPLAY_PROPERTIES"]["COMPANY"]["DISPLAY_VALUE"]): ?>
+                                    <?= $arItem["DISPLAY_PROPERTIES"]["COMPANY"]["DISPLAY_VALUE"]; ?>
+                                <? endif ?>
+                            </div>
+                        </div>
+
+                        <? //<Показывать первые 150 символов текста отзыва из анонса> ?>
+                        <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                            <div class="text-block">
+                                <?= mb_substr($arItem["PREVIEW_TEXT"], 0, 150, 'UTF-8'); ?> ...
+                            </div>
+                        <? endif; ?>
+                    </div>
+                </div>
+            </div>
+        <? endforeach; ?>
+
+        <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+            <br />
+            <?= $arResult["NAV_STRING"] ?>
+        <? endif; ?>
+    </div>
+</div>
--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -39,7 +39,7 @@ IncludeTemplateLangFile(__FILE__);
 	); ?>
 
 	<!-- /side anonse -->
-	
+
 	<!-- side wrap -->
 	<div class="side-wrap">
 		<div class="item-wrap">
@@ -60,40 +60,100 @@ IncludeTemplateLangFile(__FILE__);
 		</div>
 
 		<!-- footer rew slider box -->
-		<div class="item-wrap">
-			<div class="rew-footer-carousel">
-				<div class="item">
-					<div class="side-block side-opin">
-						<div class="inner-block">
-							<div class="title">
-								<div class="photo-block">
-									<img src="<?= SITE_TEMPLATE_PATH ?>/img/side-opin.jpg" alt="">
-								</div>
-								<div class="name-block"><a href="">Дмитрий Иванов</a></div>
-								<div class="pos-block">Генеральный директор,"Офис+"</div>
-							</div>
-							<div class="text-block">“В магзине предоставили потрясающий выбор
-								расцветок, а также, получил большую скидку по карте постоянного...</div>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="side-block side-opin">
-						<div class="inner-block">
-							<div class="title">
-								<div class="photo-block">
-									<img src="<?= SITE_TEMPLATE_PATH ?>/img/side-opin.jpg" alt="">
-								</div>
-								<div class="name-block"><a href="">Дмитрий Иванов</a></div>
-								<div class="pos-block">Генеральный директор,"Офис+"</div>
-							</div>
-							<div class="text-block">“В магазине предоставили потрясающий выбор
-								расцветок, а также, получил большую скидку по карте постоянного...</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+
+		<? $APPLICATION->IncludeComponent(
+			"bitrix:news.list",
+			"slider",
+			array(
+				"ACTIVE_DATE_FORMAT" => "j F Y",
+				"ADD_SECTIONS_CHAIN" => "N",
+				"AJAX_MODE" => "N",
+				"AJAX_OPTION_ADDITIONAL" => "",
+				"AJAX_OPTION_HISTORY" => "N",
+				"AJAX_OPTION_JUMP" => "N",
+				"AJAX_OPTION_STYLE" => "Y",
+				"CACHE_FILTER" => "N",
+				"CACHE_GROUPS" => "Y",
+				"CACHE_TIME" => "36000000",
+				"CACHE_TYPE" => "A",
+				"CHECK_DATES" => "Y",
+				"DETAIL_URL" => "",
+				"DISPLAY_BOTTOM_PAGER" => "N",
+				"DISPLAY_DATE" => "Y",
+				"DISPLAY_NAME" => "Y",
+				"DISPLAY_PICTURE" => "Y",
+				"DISPLAY_PREVIEW_TEXT" => "Y",
+				"DISPLAY_TOP_PAGER" => "N",
+				"FIELD_CODE" => array(
+					0 => "ID",
+					1 => "CODE",
+					2 => "XML_ID",
+					3 => "NAME",
+					4 => "TAGS",
+					5 => "SORT",
+					6 => "PREVIEW_TEXT",
+					7 => "PREVIEW_PICTURE",
+					8 => "DETAIL_TEXT",
+					9 => "DETAIL_PICTURE",
+					10 => "DATE_ACTIVE_FROM",
+					11 => "ACTIVE_FROM",
+					12 => "DATE_ACTIVE_TO",
+					13 => "ACTIVE_TO",
+					14 => "SHOW_COUNTER",
+					15 => "SHOW_COUNTER_START",
+					16 => "IBLOCK_TYPE_ID",
+					17 => "IBLOCK_ID",
+					18 => "IBLOCK_CODE",
+					19 => "IBLOCK_NAME",
+					20 => "IBLOCK_EXTERNAL_ID",
+					21 => "DATE_CREATE",
+					22 => "CREATED_BY",
+					23 => "CREATED_USER_NAME",
+					24 => "TIMESTAMP_X",
+					25 => "MODIFIED_BY",
+					26 => "USER_NAME",
+					27 => "",
+				),
+				"FILTER_NAME" => "",
+				"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+				"IBLOCK_ID" => "12",
+				"IBLOCK_TYPE" => "rew",
+				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+				"INCLUDE_SUBSECTIONS" => "Y",
+				"MESSAGE_404" => "",
+				"NEWS_COUNT" => "2",
+				"PAGER_BASE_LINK_ENABLE" => "N",
+				"PAGER_DESC_NUMBERING" => "N",
+				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+				"PAGER_SHOW_ALL" => "N",
+				"PAGER_SHOW_ALWAYS" => "N",
+				"PAGER_TEMPLATE" => ".default",
+				"PAGER_TITLE" => "Отзыв",
+				"PARENT_SECTION" => "",
+				"PARENT_SECTION_CODE" => "",
+				"PREVIEW_TRUNCATE_LEN" => "",
+				"PROPERTY_CODE" => array(
+					0 => "POSITION",
+					1 => "COMPANY",
+					2 => "",
+				),
+				"SET_BROWSER_TITLE" => "N",
+				"SET_LAST_MODIFIED" => "N",
+				"SET_META_DESCRIPTION" => "N",
+				"SET_META_KEYWORDS" => "N",
+				"SET_STATUS_404" => "N",
+				"SET_TITLE" => "N",
+				"SHOW_404" => "N",
+				"SORT_BY1" => "ACTIVE_FROM",
+				"SORT_BY2" => "NAME",
+				"SORT_ORDER1" => "DESC",
+				"SORT_ORDER2" => "ASC",
+				"STRICT_SECTION_CHECK" => "N",
+				"COMPONENT_TEMPLATE" => "slider"
+			),
+			false
+		); ?>
+
 		<!-- / footer rew slider box -->
 	</div>
 	<!-- /side wrap -->
