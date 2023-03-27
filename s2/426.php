2259d1a85804e9c8b787cb804b69d5be344cf829
= <=>
2023-03-27 16:26:51 +0300

    [ex1 20]

--- s2/426.php
+++ s2/426.php
@@ -2,4 +2,6 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetPageProperty("title", "Билет 3");
 $APPLICATION->SetTitle("Мебельная компания");
-?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+?>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
